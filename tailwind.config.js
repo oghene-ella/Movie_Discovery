af87f3ed0680c7205b55700c4dacfb28a3fcfc0a
oghene-ella <oghenekaro57@gmail.com>
2023-09-11 15:13:24 +0100

    added some custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,17 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		// colors: {
-		// 	button: "#BE123C"
-		// },
-		fontFamily: {
-			dm_sans: ["DM Sans", "sans-serif"],
-		},
 		extend: {
 			backgroundImage: {
 				hero_bg: "url('../src/assets/background-bg.svg')",
+			},
+			fontFamily: {
+				dm_sans: ["DM Sans", "sans-serif"],
+			},
+			colors: {
+				"my-black": "#111827",
+				"my-gray": "#6B7280",
+				"my-pink": "#BE123C",
 			},
 		},
 	},
