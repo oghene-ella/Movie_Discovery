7ff209b7ffd01bcd104072be16e08e135469f2e3
oghene-ella <oghenekaro57@gmail.com>
2023-09-11 15:12:25 +0100

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import Navbar from "./components/navbar";
 import MovieHeroData from "./components/movie_hero_data"
+import Footer from "./components/footer"
+
 const App = () => {
 
   return (
     // wrapper
-		<section className="border border-red-500 h-[120rem]">
+		<section className="border border-red-500 h-fit">
 
       {/* hero-section */}
       <section className="h-[37.5rem] bg-hero_bg flex flex-col gap-10">
         <Navbar />
         <MovieHeroData />
       </section>
+
+      {/* List of Movies */}
+      <section>
+
+      </section>
+
+      {/* footer-section */}
+      <Footer />
 
     </section>
 	);
