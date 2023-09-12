c497d42aeb24a963f7e38321ea27fb46292f330c
oghene-ella <oghenekaro57@gmail.com>
2023-09-12 13:17:28 +0100

    completed the responsiveness and favourite icon

--- src/components/moviesCard.jsx
+++ src/components/moviesCard.jsx
@@ -1,7 +1,20 @@
-const moviesCard = ({ movie }) => {
+/* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+const MovieCard = ({ movie }) => {
+	const [isClicked, setIsClicked] = useState(false);
+
+	const handleClick = () => {
+		setIsClicked(!isClicked);
+	};
+
 	return (
 		<section
-			className="movie-card font-dm_sans flex flex-col gap-2"
+			className="movie-card font-dm_sans flex flex-col gap-2 relative"
 			data-testid="movie-card"
 		>
 			<img
@@ -9,12 +22,23 @@ const moviesCard = ({ movie }) => {
 				alt={movie.title}
 				data-testid="movie-poster"
 			/>
+
 			<h2 data-testid="movie-title" className="text-xl font-bold ">
 				{movie.title}
 			</h2>
+
 			<p data-testid="movie-release-date">{movie.release_date}</p>
+
+			<span className="absolute bg-zinc-400 flex px-2 p-2 rounded-full w-fit cursor-pointer top-4 right-0 mx-3">
+				<FontAwesomeIcon
+					icon={faHeart}
+					size="lg"
+					style={{ color: isClicked ? "#E68A9F" : "#D1D5DB" }}
+					onClick={handleClick}
+				/>
+			</span>
 		</section>
 	);
 };
 
-export default moviesCard;
+export default MovieCard;
